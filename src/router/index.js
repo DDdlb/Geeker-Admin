ccb7f4402a661d620a925982cd3f473e0510f93e
DDlb <1032191254@qq.com>
2022-06-13 23:50:27 +0800

    layout样式更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,14 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 // 读取所有路由
 const metaRouters = import.meta.globEager("./modules/*.js")
-console.log(metaRouters);
 
-// * 处理路由
+// * 处理路由, 各个模块路由信息存储在routerArray中
 export const routerArray = [];
 Object.keys(metaRouters).forEach(item => {
 	Object.keys(metaRouters[item]).forEach((key) => {
 		routerArray.push(...metaRouters[item][key]);
 	});
 });
-
-console.log(routerArray);
 
 const routes = [
     {
